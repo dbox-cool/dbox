901edbcdce41d62d69f9e6c4b892cb8d421eb200
GabrielB1606 <gabrielbelisariof@gmail.com>
2025-01-13 00:23:37 -0400

    getTimeAsString date fix

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -91,6 +91,9 @@ function calculateAge(birthdate){
  * @returns {string}
 */
 function getTimeAsString(date) {
+  if(!date) return "";
+  if(!(date instanceof Date))
+    date = getDate(date);
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
   return `${hours}:${minutes}`;
