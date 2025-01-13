4e4b2c66db10ea21246a46033aee5c58d3f306d7
GabrielB1606 <gabrielbelisariof@gmail.com>
2025-01-13 08:14:22 -0400

    input jsdocs

--- components/inputs/Input.jsx
+++ components/inputs/Input.jsx
@@ -1,7 +1,12 @@
 import { forwardRef } from "react";
 import { inputfieldVariants } from "./InputfieldVariants";
 
-export const Input = forwardRef( function InputComponent({type, button, ...props}, ref){
+export const Input = forwardRef( 
+  /**
+   * @param {import("react").InputHTMLAttributes} props
+   * @returns {import("react").ReactNode}
+  */
+  function InputComponent({type, button, ...props}, ref){
     const inputCurrentVariant = inputfieldVariants({variant: button?"button":"default", type});
     return (
       <input 
