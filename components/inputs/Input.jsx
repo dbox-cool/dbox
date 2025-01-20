34d9a2becb854f9c5ba8b34841465f343d96323c
GabrielB1606 <gabrielbelisariof@gmail.com>
2025-01-20 15:44:21 -0400

    multiple

--- components/inputs/Input.jsx
+++ components/inputs/Input.jsx
@@ -8,6 +8,17 @@ export const Input = forwardRef(
   */
   function InputComponent({type, button, ...props}, ref){
     const inputCurrentVariant = inputfieldVariants({variant: button?"button":"default", type});
+    if(type == "file")
+    return (
+      <input 
+        className={type == "checkbox"? "size-4":inputCurrentVariant}
+        ref={ref}
+        type={type}
+        multiple
+        {...props}
+      /> 
+    )
+      else
     return (
       <input 
         className={type == "checkbox"? "size-4":inputCurrentVariant}
